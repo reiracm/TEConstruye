6c4f3f0b6d866089151cd8478c473855b45475bf
AaronSolera <aaron.smartinez1997@gmail.com>
2018-10-18 02:38:30 -0600

    Actualización previa al modelo final

--- TEConstruye - WEB page/MVC.js
+++ TEConstruye - WEB page/MVC.js
@@ -3,29 +3,39 @@ var main_module = angular.module('mainModule', ['ui.router']);
   main_module.config(function($stateProvider, $urlRouterProvider) {
     var states = [
       {
-        name: 'home',
+        name: 'log_in',
         url: '/',
-        templateUrl: 'home.htm',
+        templateUrl: 'pages/log_in_page.htm',
         controller: 'mainController'
       },{
         name: 'admin_account',
         url: '/admin_account',
-        templateUrl: 'admin_account.htm',
+        templateUrl: 'pages/admin_account_page.htm',
         controller: 'adminController'
       },{
         name: 'admin_account.employee_view',
         url: '/employee_view',
-        templateUrl: 'employee_page.htm',
+        templateUrl: 'views/employee_view.htm',
         controller: 'adminController'
       },{
         name: 'admin_account.project_view',
         url: '/project_view',
-        templateUrl: 'onworking.htm',
+        templateUrl: 'views/project_view.htm',
+        controller: 'adminController'
+      },{
+        name: 'admin_account.material_view',
+        url: '/material_view',
+        templateUrl: 'views/material_view.htm',
+        controller: 'adminController'
+      },{
+        name: 'admin_account.stage_view',
+        url: '/stage_view',
+        templateUrl: 'views/stage_view.htm',
         controller: 'adminController'
       },{
         name: 'admin_account.client_view',
         url: '/client_view',
-        templateUrl: 'onworking.htm',
+        templateUrl: 'views/client_view.htm',
         controller: 'adminController'
       }
     ];
@@ -57,20 +67,105 @@ var main_module = angular.module('mainModule', ['ui.router']);
 
   main_module.controller('adminController',function ($scope){
     $scope.message = "Hello user, unfortunately we are in maintaining right now. Please come here later.";
-    $scope.attributes = ['Nombre','Apellido','Codigo','Correo','Especialidad','Pago por hora','Telefono','ID'];
-    //Just depuration
-    $scope.users = [{FName:'John', LName:'Smith', Code:'1234', Email:'jsmith@teconstruye.com', Specialty:'Chapeador', Hourly_pay:'1000', Phone:'88888888',ID:'CHAPI123'}];
-    //
-    $scope.input = {FName:'', LName:'', Code:'', Email:'', Specialty:'', Hourly_pay:'', Phone:'',ID:''};
+    $scope.translate = {Fname:'Primer nombre', Sname: 'Segundo nombre', 
+                        Lname:'Apellido', Code:'Codigo', 
+                        Email:'Correo', Specialty:'Especialidad', 
+                        Hourly_pay:'Pago por hora', Password_:'Contrasena', 
+                        Phone:'Telefono', ID:'ID', IDRole:'Role ID',
+                        Name_:'Nombre', IDClient:'ID de cliente', Location_:'Localizacion', 
+                        Total_Price_:'Precio total', Start_Date_:'Fecha de inicio', End_Date:'Fecha de finalizacion',
+                        Description_:'Descripcion', Date_:'Fecha', Amount:'Cantidad', Price:'Precio'};
 
-    $scope.add= function(){
-      var temp_input = $scope.input;
-      $scope.users.push(temp_input);
-      $scope.input = {FName:'', LName:'', Code:'', Email:'', Specialty:'', Hourly_pay:'', Phone:'',ID:''};
+    $scope.employee_list = [{Fname:'John', Sname: 'Paul', Lname:'Jobs', Code:'1111', Email:'jjobs@teconstruye.com', Specialty:'Engineer', Hourly_pay:'1000', Password_:'SEXYRICOPAPU69', Phone:'88887888', ID:'1235', IDRole:'1'}]; 
+    $scope.project_list = [{Name_:'Soda la frondoza', ID:'1237', IDClient:'1235', Location_:'En la leon 13', Total_Price_:'1020200'}]; 
+    $scope.client_list = []; 
+    $scope.material_list = [];
+    $scope.stage_list = [];
+    $scope.role_list = [{Name_: 'Ingeniero', ID:'2'},{Name_: 'Administrados', ID:'1'},{Name_: 'Peon', ID:'3'}];
+    $scope.project_stages_list = [];
+    $scope.project_expense_list = [];
+
+    $scope.employee_template = {Fname:'', Sname: '', Lname:'', Code:'', Email:'', Specialty:'', Hourly_pay:'', Password_:'', Phone:'', ID:''};
+    $scope.employee_pay_hours_template = {Date:'', Hours:''};
+    $scope.project_template = {Name_:'', ID:'', IDClient:'', Location_:'', Total_Price_:''};
+    $scope.project_expense_template = {ID:'', Location_:'', Description_:'', Date_:'', Amount:'', Price:''};
+    $scope.project_stages_template = {Start_Date_:'', End_Date:''};
+    $scope.client_template = {Fname:'', Sname:'', Lname:'', Email:'', Password_:'', IDPhone:'', ID:''};
+    $scope.material_template = {Name_:'', Code:'', Price:''};
+    $scope.stage_template = {Name_:'', ID:'', Description_:''};
+
+
+    $scope.addEmployee= function(){
+      var input = $scope.employee_template;
+      input['IDRole'] = document.getElementById("employee_role_select").value;
+      alert("El empleado "+input["Fname"]+" fue agregado satisfactoriamente ");
+      $scope.employee_list.push(input);
+      $scope.employee_template = {Fname:'', Sname: '', Lname:'', Code:'', Email:'', Specialty:'', Hourly_pay:'', Password_:'', Phone:'', ID:''};
     }
 
-    $scope.go = function(page) {
+    $scope.assignEmployeePayHours= function(){
+      var input = $scope.employee_pay_hours_template;
+      input['IDProject'] = document.getElementById("assign_hours_project_select").value;
+      input['IDEmployee'] = document.getElementById("assign_hours_employee_select").value;
+      alert("Las horas trabajadas por el empleado "+input["IDEmployee"]+" fueron asignadas satisfactoriamente.");
+      $scope.employee_pay_hours_template = {Date:'', Hours:''};
+    }
+
+    $scope.addProject= function(){
+      var input = $scope.project_template;
+      alert("El proyecto "+input["Name_"]+" fue agregado satisfactoriamente.");
+      $scope.project_list.push(input);
+      $scope.project_template = {Name_:'', ID:'', IDClient:'', Location_:'', Total_Price_:''};
+    }
+    $scope.assignProjectExpense= function(){
+      var input = $scope.project_expense_template;
+      input['IDProject'] = document.getElementById("purchase_project_select").value;
+      input['IDStage'] = document.getElementById("purchase_stage_select").value;
+      alert("La compra al proyecto "+input["IDProject"]+" con numero de factura "+input["ID"]+" fue agregado a la etapa "+input["IDStage"]+"satisfactoriamente.");
+      $scope.project_expense_template = {ID:'', Location_:'', Description_:'', Date_:'', Amount:'', Price:''};
+    }
+    $scope.assignStageToProject= function(){
+      var input = $scope.project_stages_template;
+      input['IDProject'] = document.getElementById("assign_project_select").value;
+      input['IDStage'] = document.getElementById("assign_stage_select").value;
+      alert("La etapa "+input["IDStage"]+" fue asignada al proyecto "+input["IDProject"]+" satisfactoriamente.");
+      $scope.project_stages_template = {Start_Date_:'', End_Date:''};
+    }
+
+    $scope.addClient= function(){
+      var input = $scope.client_template;
+      alert("El cliente "+input["Fname"]+" fue agregado satisfactoriamente.");
+      $scope.client_list.push(input);
+      $scope.client_template = {Fname:'', Sname:'', Lname:'', Email:'', Password_:'', IDPhone:'', ID:''};
+    }
+
+    $scope.addMaterial= function(){
+      var input = $scope.material_template;
+      alert("El material "+input["Name_"]+" fue agregado satisfactoriamente a la etapa "+".");
+      $scope.material_list.push(input);
+      $scope.material_template = {Name_:'', Code:'', Price:''};
+    }
+
+    $scope.addStage= function(){
+      var input = $scope.stage_template;
+      alert("La etapa "+input["Name_"]+" fue agregado satisfactoriamente al proyecto "+".");
+      $scope.stage_list.push(input);
+      $scope.stage_template = {Name_:'', ID:'', Description_:''};
+    }
+
+    $scope.go= function(page) {
       $state.go(page);
+    };
+
+    $scope.serverAction = function(action,URL){
+      return $.ajax({
+        type: action,
+        url: URL,
+        dataType: 'jsonp',
+        success: function(data){
+          return data;
+        }
+      });
     };
   });
   //-------------------------------------------------------------------------------------
