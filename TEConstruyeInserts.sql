a56bea25364127a429468fbdfad9907ea5ea1852
DekuCool24 <dayanna.qs@gmail.com>
2018-10-17 21:53:39 -0600

    Update TEConstruyeInserts.sql
    Union de las pruebas de los procedures

--- TEConstruyeInserts.sql
+++ TEConstruyeInserts.sql
@@ -290,5 +290,248 @@ INSERT INTO MATERIAL_PER_STAGE (IDStage,	CodeMaterial,	Quantity,	Price_aprox) VA
 (20,	8877985,	1,	100000),
 (21,	8975425,	1,	20000);
 
+/*
+	Testing Procedures Gets
+*/
 
+EXECUTE usp_employees_table;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_clients_table;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_projects_table;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_purchases_table;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_stages_table;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_materials_table;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_stages_per_project_table;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_roles_table;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_works_on_table;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_manages_table;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_roles_per_employee_table;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_materials_per_stage_table;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_employees_and_roles;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_materials_and_costs_per_project_per_stage @Project_num=1;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_employee_Information_per_project @Project_Num=2;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_salary_employee_1 @ID=8,@Project=1;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+EXECUTE usp_shopping @Project_num=2;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+/*
+	Testing Procedures Inserts
+*/
+
+--Show me which is the largest ID in the table
+SELECT MAX(EMPLOYEE.ID) from EMPLOYEE
+
+--Insert a data in the Employee table
+EXECUTE usp_InsertNewEmployee 'Prueba', 'Test', 'LName', 123456, 
+'test@prueba.com', 'Civil', 2, '123', 1, 123456
+
+--Show me which is the largest ID in the table
+SELECT MAX(EMPLOYEE.ID) from EMPLOYEE
+
+--------------------------------------------------------------------------------------------------------------------------
+
+
+--Show me which is the largest ID in the table
+SELECT MAX(CLIENT.ID) from CLIENT
+
+--Insert a data in the Client table
+EXECUTE usp_InsertNewClient 'Prueba', 'Test', 'LName', 'test@prueba.com', 123, 666
+
+--Show me which is the largest ID in the table
+SELECT MAX(CLIENT.ID) from CLIENT
+
+--------------------------------------------------------------------------------------------------------------------------
+
+--	Show me which is the largest ID in the table
+SELECT MAX(PROJECT.ID) from PROJECT
+
+--	Insert a data in the Project table
+EXECUTE usp_InsertNewProject 'TEST', 123, 1, 'Prueba'
+
+--	Show me which is the largest ID in the table
+SELECT MAX(PROJECT.ID) from PROJECT
+
+--------------------------------------------------------------------------------------------------------------------------
+
+--	Show me which is the largest ID in the table
+SELECT MAX(PURCHASE.ID) from PURCHASE
+
+--	Insert a data in the Purchase table
+EXECUTE usp_InsertNewPurchase 123, 2, '20170814', 'Esto es una prueba', 1, 1, 'YOLO'
+
+--	Show me which is the largest ID in the table
+SELECT MAX(PURCHASE.ID) from PURCHASE
+
+--------------------------------------------------------------------------------------------------------------------------
+
+--	Show me which is the largest ID in the table
+SELECT MAX(STAGE.ID) from STAGE
+
+--	Insert a data in the Stage table
+EXECUTE usp_InsertNewStage 'Test', 'Esto es una prueba'
+
+--	Show me which is the largest ID in the table
+SELECT MAX(STAGE.ID) from STAGE
+
+--------------------------------------------------------------------------------------------------------------------------
+
+--	It shows me how many data there are in the material table
+SELECT COUNT (*) FROM MATERIAL
+
+--	Insert a data in the Material table
+EXECUTE usp_InsertNewMaterial 'Test', 123, 666
+
+--	It shows me how many data there are in the material table
+SELECT COUNT (*) FROM MATERIAL
+
+--------------------------------------------------------------------------------------------------------------------------
+
+--	It shows me how many data there is in the Stages_Per_Project table
+SELECT COUNT (*) FROM STAGES_PER_PROJECT
+
+--	Insert a data in the table Stages_Per_Project
+EXECUTE usp_InsertNewStages_Per_Project '20170707', '20170107', 2, 2
+
+--	It shows me how many data there is in the Stages_Per_Project table
+SELECT COUNT (*) FROM STAGES_PER_PROJECT
+
+--------------------------------------------------------------------------------------------------------------------------
+
+--	Show me which is the largest ID in the table
+SELECT MAX(ROLE_.ID) from ROLE_
+
+--	Inserted a piece of information in the Phones table
+EXECUTE usp_InsertNewRole 'TEST'
+
+--	Show me which is the largest ID in the table
+SELECT MAX(ROLE_.ID) from ROLE_
+
+--------------------------------------------------------------------------------------------------------------------------
+
+--	It shows me how many data there is in the Works_On table
+SELECT COUNT (*) FROM WORKS_ON
+
+--	Inserted a data in the Works_On table
+EXECUTE usp_InsertNewWorks_On 6, '20170701', 1, 2
+
+--	It shows me how many data there is in the Works_On table
+SELECT COUNT (*) FROM WORKS_ON
+
+--------------------------------------------------------------------------------------------------------------------------
+
+--	It shows me how much data there is in the Manages table
+SELECT COUNT (*) FROM MANAGES
+
+--	Inserted a data in the Manages table
+EXECUTE usp_InsertNewManages 2, 23
+
+--	It shows me how much data there is in the Manages table
+SELECT COUNT (*) FROM MANAGES
+
+--------------------------------------------------------------------------------------------------------------------------
+
+--	It shows me how many data there is in the table Role_Per_Employee
+SELECT COUNT (*) FROM ROLE_PER_EMPLOYEE
+
+--	Inserted a data in the table Role_Per_Employee
+EXECUTE usp_InsertNewRole_Per_Employee 23, 2
+
+--	It shows me how many data there is in the table Role_Per_Employee
+SELECT COUNT (*) FROM ROLE_PER_EMPLOYEE
+
+--------------------------------------------------------------------------------------------------------------------------
+
+--	It shows me how much data there is in the Material_Per_Stage table
+SELECT COUNT (*) FROM MATERIAL_PER_STAGE
+
+--	Inserted a data in the table Material_Per_Stage
+EXECUTE usp_InsertNewMaterial_Per_Stage 7, 8975425, 12
+
+--	It shows me how much data there is in the Material_Per_Stage table
+SELECT COUNT (*) FROM MATERIAL_PER_STAGE
+
+--------------------------------------------------------------------------------------------------------------------------
+
+/*
+	All Drops of Procedures
+*/
+
+--DROP PROCEDURE usp_InsertNewEmployee
+--DROP PROCEDURE usp_InsertNewClient
+--DROP PROCEDURE usp_InsertNewProject
+--DROP PROCEDURE usp_InsertNewPurchase
+--DROP PROCEDURE usp_InsertNewStage
+--DROP PROCEDURE usp_InsertNewMaterial
+--DROP PROCEDURE usp_InsertNewStages_Per_Project
+--DROP PROCEDURE usp_InsertNewRole
+--DROP PROCEDURE usp_InsertNewWorks_On
+--DROP PROCEDURE usp_InsertNewManages
+--DROP PROCEDURE usp_InsertNewRole_Per_Employee
+--DROP PROCEDURE usp_InsertNewMaterial_Per_Stage
+--DROP DATABASE TEConstruye
+--DROP PROCEDURE usp_employees_table;
+--DROP PROCEDURE usp_clients_table;
+--DROP PROCEDURE usp_projects_table;
+--DROP PROCEDURE usp_purchases_table;
+--DROP PROCEDURE usp_stages_table;
+--DROP PROCEDURE usp_materials_table;
+--DROP PROCEDURE usp_stages_per_project_table;
+--DROP PROCEDURE usp_roles_table;
+--DROP PROCEDURE usp_works_on_table;
+--DROP PROCEDURE usp_manages_table;
+--DROP PROCEDURE usp_roles_per_employee_table;
+--DROP PROCEDURE usp_materials_per_stage_table;
+--DROP PROCEDURE usp_employees_and_roles;
+--DROP PROCEDURE usp_materials_and_costs_per_project_per_stage @Project_num=1;
+--DROP PROCEDURE usp_employee_Information_per_project @Project_Num=2;
+--DROP PROCEDURE usp_salary_employee_1 @ID=8,@Project=1;
+--DROP PROCEDURE usp_shopping @Project_num=2;
 --DROP DATABASE TEConstruye
